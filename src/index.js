a5d092f69294ba66e7c25cd77cfb13dbd81cb98d
João Victor <santosjoao.dev@gmail.com>
2022-09-18 17:49:14 -0300

    refactor: using cookies for register requests

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import chalk from 'chalk'
 import path from 'path'
 import { fileURLToPath } from 'url';
 import { cache } from './services/cache/index.js';
+import cookieParser from 'cookie-parser'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(cookieParser())
+
 app.use('/counter.png', (req, res, next) => {
+  const wasAccessedCookie = req.cookies.wasAccessedCookie
+  if(wasAccessedCookie){
+    next()
+    return
+  }
+  res.cookie('wasAccessedCookie', true, {maxAge: 9999999})
   console.log('user requested the counter image', req.ip)
   cache.set(`visitors#${req.ip}`)
   cache.emit('addingAddress')
